0f87b030914123a780508858f80b74a7d53e6c32
thiagoxts40 <thiagoxts@gmail.com>
2025-08-18 20:12:57 -0300

    primerio comit

---
+++ api_noite.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: application/json. charset=UTF-8');
+header('Acess-Control-Allow-Origin *');
+header('Acess-Control-Allow-Methods: POST, PUT');
+header('Acess-Control-Allow-Originheaders: Content-Type');
+?>
